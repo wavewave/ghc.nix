9f97fd280806adb3c7b7daa61a98829770217e87
mangoiv <mail@mangoiv.com>
2022-10-31 00:27:19 +0100

    finish rmeoving niv specific code, added flake-compat stuff

--- flake.nix
+++ flake.nix
@@ -3,22 +3,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     # NOTE: matches nixpkgs-unstable from sources.nix
-    # This is needed until the bootGHC version upgrades to a version that 
-    # nixpkgs-unstable contains an hls for 
     nixpkgs-unstable.url = "github:nixos/nixpkgs/e14f9fb57315f0d4abde222364f19f88c77d2b79";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable }: with nixpkgs.lib; let
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, ... }: with nixpkgs.lib; let
     supportedSystems = nixpkgs.lib.systems.flakeExposed;
     perSystem = genAttrs supportedSystems;
 
     pkgsFor = system: import nixpkgs { inherit system; };
     unstablePkgsFor = system: import nixpkgs-unstable { inherit system; };
+
+    all-cabal = {
+      owner = "commercialhaskell";
+      repo = "all-cabal-hashes";
+      rev = "f4b3c68d6b5b128503bc1139cfc66e0537bccedd";
+      sha256 = "1x341yzi40xr6dxx2dvah4g943daih1y6dm4jh1d5w9fjff9v5s7";
+    };
   in
   {
     devShells = perSystem (system: {
-      default = import ./. {
+      default = import ./ghc.nix {
         inherit system;
+        all-cabal-hashes = with all-cabal; builtins.fetchurl {
+          inherit sha256;
+          url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+          name = "all-cabal-hashes-${rev}.tar.gz";
+        };
         withHadrianDeps = true;
         withIde = true;
         nixpkgs = pkgsFor system;
